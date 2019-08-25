f5d278f58ae722d56c89f81841c700a91034570c
EC2 Default User <ec2-user@ip-172-31-16-77.ap-southeast-1.compute.internal>
2019-08-25 03:07:58 +0000

    完成、あとはジャバスクリプト

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,45 @@
 class PostsController < ApplicationController
   
+before_action :move_to_index, except: [:please_login, :index, :show]  
+  
   def index
-
+      @posts = Post.includes(:user).order("id DESC").page(params[:page]).per(8)
   end
+  
+  def new
+    @post = Post.new
+  end
+
+  def create 
+    Post.create(text: post_params[:text], title: post_params[:title], user_id: current_user.id)
+    redirect_to action: :index
+  end
+  
+  def please_login
+  end
+  
+def destroy
+  post = Post.find(params[:id])
+  if post.user_id == current_user.id
+    post.destroy
+  end
+  redirect_to action: :index
+end
+
+def edit
+  @post = Post.find(params[:id])
+end
+
+
+
+private
+
+def move_to_index
+  redirect_to action: :please_login unless user_signed_in?
+end
+
+
+def post_params
+  params.permit(:title , :text)
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root 'posts#index'
+  get 'posts/please_login' => 'posts#please_login'
   resources :posts
+  get   'users/:id'   =>  'users#show' 
+
+
 end
